be40d0deedb449cf6955feed81296b6f6a68b23f
Axel Clark <axel.clark@pm.me>
2020-01-18 16:41:35 -0500

    Add plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,14 @@ module.exports = {
     author: `@taylorbryant`
   },
   plugins: [
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`
+      }
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-manifest`,
