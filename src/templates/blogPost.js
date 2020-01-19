9064a2aca2526acb8bc146982269c748135c647c
Axel Clark <axel.clark@pm.me>
2020-01-18 21:13:08 -0500

    Build blog post template

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,8 +1,28 @@
 import React from "react";
 import { graphql } from "gatsby";
 
-const Template = props => {
-  return <div>Blog Post Here</div>;
+const Template = ({ data }) => {
+  const { markdownRemark } = data;
+  const title = markdownRemark.frontmatter.title;
+  const html = markdownRemark.html;
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <div className="blogpost" dangerouslySetInnerHTML={{ __html: html }} />
+    </div>
+  );
 };
+
+export const query = graphql`
+  query($pathSlug: String!) {
+    markdownRemark(frontmatter: { path: { eq: $pathSlug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`;
 
 export default Template;
