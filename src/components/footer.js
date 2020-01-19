bdc8cb9dcdf3f0f89b443afb3bb587c4dfe535bd
Axel Clark <axel.clark@pm.me>
2020-01-19 12:46:53 -0500

    Remove colors from footer/header

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-blue-700">
+    <footer className="bg-gray-700">
       <nav className="flex justify-between max-w-4xl mx-auto p-4 md:p-4 text-sm">
         <p className="text-white">
           Created by{` `}
