8d92007d4d1bce249830fa37889bd51851c036c2
Axel Clark <axel.clark@pm.me>
2020-01-25 15:39:22 -0500

    Add styles at md breakpoint

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="bg-gray-700">
-      <nav className="flex justify-between max-w-4xl mx-auto p-4 md:p-4 text-sm">
+      <nav className="flex justify-between max-w-2xl mx-auto p-4 md:p-4 text-sm">
         <p className="text-white">
           Created by{` `}
           <a
--- src/components/header.js
+++ src/components/header.js
@@ -15,7 +15,7 @@ function Header() {
 
   return (
     <header className="bg-gray-700">
-      <div className="flex flex-wrap items-center justify-between max-w-4xl mx-auto p-4 md:p-4">
+      <div className="flex flex-wrap items-center justify-between max-w-2xl mx-auto p-4 md:p-4">
         <Link className="flex items-center no-underline text-white" to="/">
           <span className="font-bold text-xl tracking-tight">
             {site.siteMetadata.title}
