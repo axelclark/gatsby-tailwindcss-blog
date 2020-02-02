376a803a2e400b1b67cd063455ed4df8f3a43d20
Axel Clark <axel.clark@pm.me>
2020-02-02 12:12:39 -0500

    Update footer
    * Update footer to link to project on Github
    * Closes #15

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,13 +5,14 @@ function Footer() {
     <footer className="bg-indigo-900">
       <nav className="flex justify-between max-w-2xl mx-auto p-4 md:p-4 text-sm">
         <p className="text-white">
-          Created by{` `}
+          See the{` `}
           <a
-            className="font-bold no-underline text-white"
+            className="font-bold text-white hover:underline"
             href="https://github.com/axelclark"
           >
-            Axel Clark
+            code
           </a>
+          {` `}for this site on Github.
         </p>
       </nav>
     </footer>
