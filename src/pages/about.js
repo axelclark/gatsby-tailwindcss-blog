dd7e17dee83eec4aca89c184444741f1e48293ae
Axel Clark <axel.clark@pm.me>
2020-02-02 18:20:19 -0500

    Add style to About headline

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,7 +13,10 @@ function AboutPage() {
 
       <section className="flex flex-col items-center">
         <div className="markdown">
-          <h1>Hi, my name is Axel.</h1>
+          <h1>
+            Hi, my name is{` `}
+            <span className="bg-indigo-900 text-gray-100 px-2 py-1">Axel</span>.
+          </h1>
           <p>
             I live and work in New Jersey. For my day job, I'm an officer in the
             Air Force who manages procurement. I program in my off-time for fun,
